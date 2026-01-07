bee88adf3376209369410354d69b9a58d12f75e5
Haseebx162006 <haseebahmad0160@gmail.com>
2026-01-07 18:47:40 +0500

    Authentication

--- Backend/app/core/security.py
+++ Backend/app/core/security.py
@@ -2,6 +2,7 @@ from passlib.context import CryptContext
 from jose import jwt, JWTError
 from datetime import datetime, timedelta
 pwd_context= CryptContext(schemes=["bcrypt"],deprecated="auto")
+from config import ALGORITHM , ACCESS_TOKEN_EXPIRE, SECRET_KEY
 
 def verify_password(plain_password, hash_password):
     pwd_context.verify(plain_password,hash_password)
@@ -10,7 +11,17 @@ def hash_password(password: str):
     pwd_context.hash(password)
 
 
-def create_access_token(data: dict):
+def create_access_token(data: dict, expire_time:timedelta):
     to_encode=data.copy()
-    
+    if expire_time:
+        expire=datetime.utcnow()+expire_time
+    else:
+        expire=datetime.utcnow()+timedelta(minutes=30)
+        
+    to_encode.update({
+        
+        "exp":expire
+    })
+    encoded_jwt=jwt.encode(to_encode,SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
     
